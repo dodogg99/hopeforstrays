a3bd1d132e6bfe1081902d523fc392ae94ff02f2
robert <dodogg99@gmail.com>
2024-04-11 12:55:05 +0800

    add body-parser library

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express";
 import createError from "http-errors";
 import logger from 'morgan';
 import path, { dirname } from "path";
+import bodyParser from "body-parser";
 import { fileURLToPath } from "url";
 import indexRouter from "./route/index.js";
 
@@ -14,6 +15,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(logger('dev'));
 app.use('/', indexRouter);
 
