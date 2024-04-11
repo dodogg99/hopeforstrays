445339b78f30ff30c6b3fee4718b0bbdfb79d8fa
robert <dodogg99@gmail.com>
2024-04-11 18:50:51 +0800

    console log type of req.body

--- route/index.js
+++ route/index.js
@@ -71,6 +71,7 @@ router.post('/newebpay_return', function (req, res, next) {
 // 確認交易：Notify
 router.post('/newebpay_notify', function (req, res, next) {
   console.log('req.body notify data', req.body);
+  console.log(typeof(req.body));
   const response = JSON.parse(req.body);
   console.log(response.TradeInfo);
   console.log(createAesDecrypt(response.TradeInfo));
